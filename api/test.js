acba87a18b1b2b036c96472de0dd963ed70513b5
Christian Moss <chrismoss411@gmail.com>
2025-10-30 22:59:28 +0000

    Add detailed logging

--- api/test.js
+++ api/test.js
@@ -101,6 +101,11 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+  // Prevent caching of API responses
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
